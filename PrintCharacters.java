fd387b8e3623ecdd3365e45fc2d21c6c95680663
Achal192001 <achalchittalwar@gmail.com>
2025-12-08 14:33:51 +0530

    Add files via upload
    Day1 String Basic Programs

---
+++ PrintCharacters.java
@@ -0,0 +1,8 @@
+public class PrintCharacters {
+    public static void main(String[] args) {
+        String str = "Achal";
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+    }
+}
